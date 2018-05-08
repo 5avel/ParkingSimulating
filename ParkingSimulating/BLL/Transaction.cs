2fe2d87a36304f003fac82aeeb132215fa59d355
Bolubash Pavel <5avel@hotmail.com>
2018-05-08 17:18:00 +0300

    Created Transaction

---
+++ ParkingSimulating/BLL/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingSimulating.BLL
+{
+    public class Transaction
+    {
+        public DateTime DateTime { get; private set; }
+        public string CarLicensePlate { get; private set; }
+        public decimal Debited { get; private set; }
+
+        public Transaction(string carLicensePlate, decimal debited)
+        {
+            this.DateTime = DateTime.Now;
+            this.CarLicensePlate = carLicensePlate;
+            this.Debited = debited;
+        }
+    }
+}
